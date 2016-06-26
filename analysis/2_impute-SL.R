290a098a7b7cc692bc9612b9b91524cfe4aee476
Nima Hejazi <nima.hejazi7@gmail.com>
2016-06-26 13:56:09 -0700

    add working CV-SL code

--- analysis/2_impute-SL.R
+++ analysis/2_impute-SL.R
@@ -7,38 +7,29 @@
 ###==============================###
 
 library(SuperLearner)
+SL.library <- c("SL.mean", "SL.glm", "SL.bayesglm", "SL.stepAIC", "SL.gam",
+                "SL.randomForest", "SL.nnet")
+CV_folds = 10
 
-full_data <- micedata[, c(5:26), with = FALSE]
+obs_O <- as.data.frame(micedata[, c(5:26), with = FALSE])
+
+yfit <- list()
 
 for (idx in 1:length(missvars)) {
-  y_idx <- which(colnames(full_data) == missvars[idx])
-  y <- subset(full_data, select = y_idx)
-  X <- subset(full_data, select = -y_idx)
+  set.seed(0)
+  #organize data into learning matrix and prediction set
+  y_idx <- which(colnames(obs_O) == missvars[idx])
+  y <- as.vector(subset(obs_O, select = y_idx)[, 1])
+  X <- subset(obs_O, select = -y_idx)
 
-  
+  #get rid of all missing values remaining in training data
+  X[is.na(X)] <- 0  #many other options for imputation, zero just to start
 
-  for (icol in 1:ncol(X)) {
-    if (sum(X[, icol]) == "NA") {
-      na_idx <- which(
-    }
-  }
-
+  #use SuperLearner with 10-fold cross-validation to predict missing gene Y
+  yfit.SL <- CV.SuperLearner(y, X, V = CV_folds, family = gaussian(),
+                             SL.library = SL.library, verbose = TRUE)
+  yfit[[idx]] <- yfit.SL$SL.predict
 }
 
-X=Xdat
-xp=dim(Xdat)[2]
-
-if(xp > 0) {
-  sna=apply(Xdat,2,sum.na)
-  nmesX=names(Xdat)
-
-  for(k in 1:xp){
-    if(sna[k] > 0) {
-        ix=as.numeric(is.na(Xdat[,k])==F)
-        Xdat[is.na(Xdat[,k]),k]=0
-        Xdat=data.frame(Xdat,ix)
-        nmesX=c(nmesX,paste("Imiss_",nmesX[k],sep=""))
-    }
-  }
-  names(Xdat)=nmesX
-}
+# to be continued...
+# need to test whether this actually does what it seems to
