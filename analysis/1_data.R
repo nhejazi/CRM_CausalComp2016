ba6cf71e71477d7215b49ff1f68c5e2a51402569
Nima Hejazi <nima.hejazi7@gmail.com>
2016-06-29 01:04:46 -0700

    add SL and plots

--- analysis/1_data.R
+++ analysis/1_data.R
@@ -10,7 +10,6 @@
 proj_path <- "~/git_repos/CRM_CausalComp2016/analysis/"
 data_path <- "~/git_repos/CRM_CausalComp2016/data/"
 library(data.table)
-library(ggplot2)
 
 # load data using included helper script
 #source("load_mouse_data.R")  #includes example causal DAG
@@ -19,6 +18,9 @@ codebook <- data.table(readRDS(paste0(data_path, "variable_names.RDS")))
 
 # examine missing-ness in observed full data structure
 knockout <- subset(micedata, is.na(rowSums(micedata[, c(5:26), with = FALSE])))
-missvars <- colnames(micedata)[which(is.na(colSums(micedata[, c(5:26),
-                                                   with = FALSE])))]
+#missvars <- colnames(micedata)[which(is.na(colSums(micedata[, c(5:26),
+#                                                   with = FALSE])))]
+miss_subj <- which(is.na(rowSums(as.data.frame(micedata)[, 5:26])))
 missinfo <- (codebook$nam)[which(codebook$ph %in% missvars)]
+
+#EndScript
--- analysis/2_cvsl.R
+++ analysis/2_cvsl.R
@@ -3,30 +3,39 @@
 ### CRM Workshop and Conference  ###
 ### Montreal, Canada, July 2016  ###
 ### Causal Inference Competition ###
-### Script #2: Impute using SL   ###
+### Script #2: Imputation & SL   ###
 ###==============================###
 
-library(SuperLearner)
-SL.library <- c("SL.mean", "SL.glm", "SL.bayesglm", "SL.stepAIC", "SL.gam",
-                "SL.randomForest", "SL.nnet")
+library(SuperLearner); library(mice)
+SL.library <- c("SL.loess", "SL.glm", "SL.bayesglm", "SL.stepAIC", "SL.gam",
+                "SL.mean")
 CV_folds = 10
 
+# generate observed data and find variables with missingness
 obs_O <- as.data.frame(micedata[, c(5:26), with = FALSE])
+obs_O.missing <- md.pattern(obs_O)
+varsNA <- names(which(is.na(colSums(obs_O, na.rm = FALSE))))
 
-yfit <- list()
+# imputation and variable scaling
+obs_O.impute <- obs_O
+obs_O.impute[is.na(obs_O.impute)] <- 0  #Zero replacement, try empirical Bayes
+O.imputed.scaled <- scale(obs_O.impute)
 
-for (idx in 1:length(missvars)) {
+yfit <- list()  #store predicted values over all folds of cross-validation
+
+for (idx in 1:length(varsNA)) {
   set.seed(0)
   #get prediction covariate
-  y <- subset(obs_O, select = names(which(is.na(colSums(obs_O))))[idx])
+  y <- subset(O.imputed.scaled, select = varsNA[idx])
   y <- as.vector(y[, 1])
 
   #get covariates for training
-  X <- subset(obs_O,
-              select = names(obs_O) != names(which(is.na(colSums(obs_O))))[idx])
+  X <- subset(O.imputed.scaled,
+              select = colnames(O.imputed.scaled) !=varsNA[idx])
+  X <- as.data.frame(X)
 
   #get rid of all missing values remaining in training data
-  X[is.na(X)] <- 0  #many other options for imputation, zero just to start
+  #X[is.na(X)] <- 0  #many other options for imputation, zero just to start
 
   #use SuperLearner with 10-fold cross-validation to predict missing gene Y
   yfit.SL <- CV.SuperLearner(y, X, V = CV_folds, family = gaussian(),
@@ -34,5 +43,43 @@ for (idx in 1:length(missvars)) {
   yfit[[idx]] <- yfit.SL$SL.predict
 }
 
-# to be continued...
-# need to test whether this actually does what it seems to
+# find indices of missing values for covariates with missingness
+indNA <- list()
+for (i in 1:sum(is.na(colMeans(obs_O)))) {
+  indNA[[i]] <- which(is.na(obs_O[,varsNA[i]]))
+}
+
+# un-normalize predicted values from the SuperLearner
+yfit_untrans <- list()
+for (i in 1:length(yfit)) {
+  yfit_untrans[[i]] <- (yfit[[i]]*sd(obs_O[, varsNA[i]], na.rm = TRUE)) +
+                       mean(obs_O[, varsNA[i]], na.rm = TRUE)
+}
+
+# replace missing values in original data structure with predicted values
+for (i in 1:length(yfit_untrans)) {
+  obs_O[which(is.na(obs_O[,varsNA[i]])), varsNA[i]] <- yfit_untrans[[i]][which(is.na(obs_O[, varsNA[i]]))]
+}
+
+# clean up workspace a bit
+rm("i", "idx", "O.imputed.scaled", "obs_O.impute", "obs_O.missing", "yfit.SL",
+   "X", "y", "yfit_untrans")
+
+#EndScript
+
+
+# visualization stuff -- make into new script
+library(ggplot2)
+library(plotly)
+Sys.setenv("plotly_username" = "nhejazi")
+Sys.setenv("plotly_api_key" = "73h2ov8u7o")
+
+for (i in 1:length(varsNA)) {
+  p <- ggplot(obs_O, aes_string(x = varsNA[i]))
+  p <- p + ggtitle(paste0("Histogram of ",
+                          as.character(subset(codebook, ph == varsNA[i])$nam)))
+  p <- p + geom_histogram(aes(fill = ..count..))
+  p <- p + scale_fill_gradient("Count", low = "green", high = "red")
+  ggplotly(p)
+  plotly_POST(p, paste0("Histogram of ", as.character(varsNA[i])))
+}
